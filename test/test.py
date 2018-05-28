3a148dd0cfbd04c144f61fbf6f2dc7b7f0e82d93
592901924@qq.com <592901924@qq.com>
2018-05-28 10:58:00 +0800

    github commit ：update UCAS

---
+++ test/test.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+# !/usr/bin/python3
+# @version: V
+# @author: 飞翔的卡夫卡
+# @contact: 592901924@qq.com
+# @site: 
+# @software: PyCharm
+# @file: test.py
+# @time: 2018/5/25 14:40
+# @python：python3.6
+ 
